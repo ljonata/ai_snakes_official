0d12600be48de4ed657d2491e70d49933d4a9069
Luiz Jonata Pires de Araujo <ljonata@users.noreply.github.com>
2021-01-14 15:49:23 +0300

    Converting Java to Python.

--- python_snakes/coordinate.py
+++ python_snakes/coordinate.py
@@ -1,3 +1,37 @@
 class Coordinate:
     x = 0
     y = 0
+    
+    def __init__(self, newx, newy):
+        self.x = newx
+        self.y = newy
+    
+    def __eq__(self, other): 
+        if not isinstance(other, Coordinate):
+            # don't attempt to compare against unrelated types
+            return False
+        if self.x == other.x and self.y == other.y:
+            return True
+        return False
+    
+    # Check whether coordinate is in bounds of board
+    # Return True if this coordinate is in bounds
+    def in_bounds(self, maze_width, maze_height):
+        if x >= 0 and y >= 0 and x < maze_width and y < maze_height:
+            return True
+        else:
+            return False
+
+    # Move coordinate in direction
+    # Return a moved coordinate
+    def move_to(self, direction):
+        c = Coordinate(self.x, self.y)
+        if direction == Direction.UP:
+            c.y = c.y + 1 
+        elif direction == Direction.DOWN:
+            c.y = c.y - 1
+        elif direction == Direction.LEFT:
+            c.x = c.x - 1
+        elif direction == Direction.RIGHT:
+            c.x = c.x + 1
+        return c
--- python_snakes/snake.py
+++ python_snakes/snake.py
@@ -4,11 +4,38 @@ class Snake:
     maze_width = 0
     maze_height = 0
     
-    
-    
     # head is of type Coordinate
     # width and height are integers
     def __init__(self, head, width, height):
         self.body.append(head)
         self.maze_width = width
         self.maze_height = height
+        
+    # Check whether head collides with another snake
+    # other_snake = body of the opponent's snake
+    # Return whether this snake collides with another snake
+    def headCollidesWith(self, other_snake):
+        if self.body[0] in other_snake.body:
+            return True
+        else:
+            return False
+    
+    # Move snake in the given direction
+    # d is an instance of direction where should snake crawl to
+    # grow is boolean, whether the snake eat an apple
+    # Return False if this snake collides with itself or maze bounds
+    def moveTo(self, direction, grow):
+        head_copy = Coordinate(self.body[0].x, self.body[0].y)
+        new_head = head_copy.move_to(direction)
+
+        if not new_head.in_bounds():
+            return False # Left maze
+
+        if not grow:
+            del self.body[-1]
+        
+        if new_head in self.body:
+            return False # Collided with itself
+
+        body.insert(0, new_head)
+        return True
